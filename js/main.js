6b94764c892ed80992d026bcbd440fdaafa3a483
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-15 05:41:02 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
-    }
-  ]
-
-  function loadIndex() {
-      const ol = document.querySelector("#linksList");
-
-      links.forEach( link => {
-          const li = document.createElement("li");
-          const href= document.createElement("a");
-          href.setAttribute("href", link.url);
-          href.innerText = link.label;
-
-          li.appendChild(href);
-          ol.appendChild(li);
-      })
+  {
+    label: "Week1 notes",
+    url: "week1/index.html"
+  },
+  {
+    label: "Week2 notes",
+    url: "week2/index.html"
   }
+]
+
+function loadIndex() {
+  const ol = document.querySelector("#linksList");
+
+  links.forEach(link => {
+    const li = document.createElement("li");
+    const href = document.createElement("a");
+    href.setAttribute("href", link.url);
+    href.innerText = link.label;
+
+    li.appendChild(href);
+    ol.appendChild(li);
+  })
+}
