685c00a14fa623e48a10f4d43fadbb1ad25593cc
JulioCaballero63 <juliocl_14@hotmail.com>
2022-02-17 16:45:08 -0700

    fixed link

---
+++ todo/js/utils.js
@@ -0,0 +1,13 @@
+function activeFilter(todos) {
+    return todos.filter(todo => {
+        return !todo.completed
+    })
+}
+function completedFilter(todos) {
+    return todos.filter(todo => {
+        return todo.completed
+    })
+}
+export default {
+    activeFilter, completedFilter
+}
