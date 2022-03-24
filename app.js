ecd7bcc204668bd79a3880aee5145c8b992df044
JulioCaballero63 <juliocl_14@hotmail.com>
2022-03-24 06:15:23 -0600

    fist commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const PORTS = process.env.PORT || 3000;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+const weekRoutes = require('./routes/weeklyRoute');
+const taRoutes = require('./routes/taRoute');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(weekRoutes);
+app.use(taRoutes);
+
+app.use(errorController.get404);
+
+app.listen(PORTS);
